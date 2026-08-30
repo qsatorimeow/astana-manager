d03a7fe02bcd2fba1df7d9e7d981392dcbca8e4b
qsatorimeow <qsatorimeow@gmail.com>
2026-08-30 16:54:52 +0500

    Delete kv.ts

--- kv.ts
+++
@@ -1,7 +0,0 @@
-// Подключение к Upstash Redis.
-import { Redis } from "npm:@upstash/redis@1.34.3";
-
-export const redis = new Redis({
-  url: Deno.env.get("UPSTASH_REDIS_REST_URL")!,
-  token: Deno.env.get("UPSTASH_REDIS_REST_TOKEN")!,
-});
